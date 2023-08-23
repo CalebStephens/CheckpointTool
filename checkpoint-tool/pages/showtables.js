4fb98ec8c7cc57816bc42a7e5b673b89826ae63c
Marco Koen <neokocram@gmail.com>
2023-08-23 17:20:54 +1200

    Add two pages

---
+++ checkpoint-tool/pages/showtables.js
@@ -0,0 +1,13 @@
+import ShowTables from '@/components/ShowTables'
+
+
+export default function ShowTables() {
+    return (
+      <main
+        className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+      >
+        <ShowTables/>
+  
+      </main>
+    )
+  }
