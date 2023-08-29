f27629c6c2806441983e50099f99927ed3092a8e
MarcoKoen <neokocram@gmail.com>
2023-08-29 12:16:55 +1200

    3 tables done

--- checkpoint-tool/pages/index.js
+++ checkpoint-tool/pages/index.js
@@ -1,4 +1,7 @@
 import SideBar from '@/components/SideBar'
+import LabsCompleted from '@/components/labsCompleted'
+import ShowTables from '@/components/ShowTables'
+import DangerZone from '@/components/DangerZone'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,9 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
-      <SideBar />
+      <DangerZone />
 
     </main>
   )
