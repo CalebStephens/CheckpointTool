5a5dccecb43e5a235128b72588a24193727e9d35
CalebStephens <calebtaylorstephens@gmail.com>
2023-08-31 10:40:55 +1200

    start of student home screen

---
+++ checkpoint-tool/pages/student/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-screen h-screen'>
+        <h1>Home</h1>
+        <div className='flex justify-evenly'>
+            <div>Test</div>
+            <div>Test</div>
+
+        </div>
+
+    </div>
+   
+  )
+}
+
+export default Home
