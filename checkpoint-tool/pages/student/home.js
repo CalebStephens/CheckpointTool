f5081dc57bbc019b225c383a7fb788ddc316ac63
CalebStephens <calebtaylorstephens@gmail.com>
2023-09-03 19:42:58 +1200

    created home page for student login

--- checkpoint-tool/pages/student/home.js
+++ checkpoint-tool/pages/student/home.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faListCheck } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className='w-screen h-screen'>
-        <h1>Home</h1>
-        <div className='flex justify-evenly'>
-            <div>Test</div>
-            <div>Test</div>
-
-        </div>
-
+    <div className="w-screen h-screen">
+      <div className="flex p-6 justify-evenly gap-5 items-center w-full flex-col md:flex-row">
+        <Link href="./checkpoint"
+          className="w-96 h-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col-reverse">
+          <div className="w-full p-6 dark:bg-gray-700">
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Mark Checkpoint Complete</h2>
+            <p className="text-gray-700 dark:text-gray-400">Mark a checkpoint as complete.</p>
+          </div>
+          <FontAwesomeIcon icon={faCheck} className="bg-repeat text-9xl mx-auto" />
+        </Link>
+        <Link href=""
+          className="w-96 h-80 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col-reverse">
+          <div className="w-full p-6 dark:bg-gray-700">
+            <h2 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Completed Checkpoints</h2>
+            <p className="text-gray-700 dark:text-gray-400">See checkpoints you have completed.</p>
+          </div>
+          <FontAwesomeIcon icon={faListCheck} className="bg-repeat text-9xl mx-auto" />
+        </Link>
+      </div>
     </div>
-   
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
