48d6a6f1907ecb4d85abe435fc92dd7f5b0ecefc
CalebStephens <calebtaylorstephens@gmail.com>
2023-09-13 20:57:35 +1200

    grid surrounds layed out

--- checkpoint-tool/pages/student/checkpoint.js
+++ checkpoint-tool/pages/student/checkpoint.js
@@ -1,18 +1,47 @@
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, {useState, useEffect} from "react";
 
 const Checkpoint = () => {
+  const [student, setStudent] = useState({
+    name: "",
+    lab: "",
+    password: ""
+  });
+
+  const router = useRouter();
+
+  const auth = (e) => {
+    e.preventDefault();
+    if(student.name === "" || student.lab === ""){
+      alert("Please select a student and lab");
+      return;
+    }
+
+    const pass = `${student.lab}5${parseInt(student.lab) * 5}`
+    
+    pass === student.password ? router.push('./labDescribe') : alert("Incorrect Password")
+
+  }
+
+  useEffect(() => {
+    console.log(student)
+  }, [student]);
+
+
+
   return (
     <div className="p-6">
       <form>
         <div className="mb-6">
-          <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <label htmlFor="student" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Student Name:
           </label>
           <select
             id="student"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option selected>Choose a Student</option>
+            onChange={(e) => setStudent({ ...student, name: e.target.value })}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option value="">Choose a Lab</option>
             <option value="jim-bob">Jim Bob</option>
             <option value="bobby-bobbinson">Bobby Bobbinson</option>
             <option value="marko-coen">Marko Coen</option>
@@ -20,13 +49,14 @@ const Checkpoint = () => {
         </div>
         <div className="mb-6 flex flex-row gap-6 w-full">
           <div className="w-1/2">
-            <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label htmlFor="lab" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Lab Number:
             </label>
             <select
               id="lab"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option selected>Choose a Lab</option>
+              onChange={(e) => setStudent({ ...student, lab: e.target.value })}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option value="">Choose a Lab</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -34,24 +64,26 @@ const Checkpoint = () => {
             </select>
           </div>
           <div className="w-1/2">
-            <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Lab Password:
             </label>
             <input
               type="password"
               id="password"
+              onChange={(e) => setStudent({ ...student, password: e.target.value })}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required
             />
           </div>
         </div>
         
-        <Link
-          type="submit"
-          href="./labDescribe"
+        <button
+          
+          onClick={auth}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Mark Complete
-        </Link>
+        </button>
+
       </form>
     </div>
   );
--- checkpoint-tool/pages/student/labDescribe.js
+++ checkpoint-tool/pages/student/labDescribe.js
@@ -1,36 +1,120 @@
 import DescribeGrid from "@/components/student/describeGrid";
-import React, {useState} from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const LabDescribe = () => {
   const [index, setIndex] = useState(0);
   const [reset, setReset] = useState(false);
-  const [coords, setCoords] = useState('');
+  const [coords, setCoords] = useState("");
   const [ans, setAns] = useState([]);
-
+  const [completed, setCompleted] = useState(false);
 
   const qs = [
-    "question1",
-    "question2",
-    "question3"
-  ]
+    {
+      question: "Use the grid to choose the point that best describes your opinion of todays lab.",
+      labels: {
+        x: {
+          left: "Easy",
+          right: "Hard",
+        },
+        y: {
+          top: "Interesting",
+          bottom: "Boring",
+        },
+      },
+    },
+    {
+      question: "Use the grid to choose the point that best describes your opinion of todays lab.",
+      labels: {
+        x: {
+          left: "Content was all new",
+          right: "Content was all familiar",
+        },
+        y: {
+          top: "I could confidently start on my own",
+          bottom: "I needed help to get started",
+        },
+      },
+    },
+    {
+      question: "Use the grid to choose the point that best describes your opinion of todays lab.",
+      labels: {
+        x: {
+          left: "My Programming Skills have not imporved",
+          right: "My Programming Skills have imporved",
+        },
+        y: {
+          top: "I feel triumphant",
+          bottom: "I feel frustrated",
+        },
+      },
+    },
+  ];
 
   const nextQuestion = () => {
-    setAns([...ans, coords]);
-    setCoords('');
-    setReset(!reset);
-    setIndex(index + 1);
-  }
+    if(!coords.length){
+      alert("Please select a point on the grid");
+      return
+    }
+    if (index === qs.length - 1) {
+      setCompleted(true);
+    } else {
+      setAns([...ans, coords]);
+      setCoords("");
+      setReset(!reset);
+      setIndex(index + 1);
+    }
+  };
 
   return (
     <div className="w-full h-screen flex justify-center items-center flex-col space-y-4">
-      <h2>{qs[index]} {coords}</h2>
-      <p>{ans}</p>
-
-          <DescribeGrid setCoords={setCoords} reset={reset}/>
-          <div className="flex justify-evenly items-center space-x-4 w-full">
-          <button type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Skip</button>
-        <button type="button" onClick={nextQuestion} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+      {completed ? (
+        <div className="flex justify-center items-center flex-col space-y-4">
+          <h2 className="text-5xl font-extrabold">Submitted</h2>
+          <Link
+              type="button"
+              href={"/student/home"}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              Go Home
+            </Link>
         </div>
+      ) : (
+        <>
+          <h2>{qs[index].question}</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td className="w-48 text-right">
+                  <div>{qs[index].labels.x.left}</div>
+                </td>
+                <td className="">
+                  <table>
+                    <caption style={{ captionSide: "top" }}>{qs[index].labels.y.top}</caption>
+                    <caption style={{ captionSide: "bottom" }}>{qs[index].labels.y.bottom}</caption>
+                    <DescribeGrid setCoords={setCoords} reset={reset} />
+                  </table>
+                </td>
+                <td className="w-48">
+                  <div>{qs[index].labels.x.right}</div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="flex justify-evenly items-center space-x-4 w-full">
+            <button
+              type="button"
+              className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+              Skip
+            </button>
+            <button
+              type="button"
+              onClick={nextQuestion}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              Submit
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
