37a6da557d04e39c3987e5d6736301b783836b4e
CalebStephens <calebtaylorstephens@gmail.com>
2023-10-02 21:46:43 +1300

    app utils

--- checkpoint-tool/backend/app.js
+++ checkpoint-tool/backend/app.js
@@ -1,8 +1,14 @@
-// Import the Express module
-import express from 'express';
+import dotenv from 'dotenv';
+import express, { urlencoded, json } from 'express';
 import cors from "cors"; 
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import compression from 'compression';
+import cacheRoute from './middleware/cacheRoute.js';
+
+import auth from './routes/v1/auth.js';
+import tool from './routes/v1/tools.js';
+import papers from './routes/v1/papers.js';
 
 dotenv.config();
 const app = express();
@@ -16,19 +22,23 @@ const limiter = rateLimit({
     max: 100 // limit each IP to 100 requests per windowMs
   });
   
-  app.use(limiter);
-
+app.use(limiter);
 app.use(cors()); 
 app.use(helmet());
+app.use(compression());
+app.use(cacheRoute);
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
-// Create a GET route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+
+app.use(`/${BASE_URL}/${CURRENT_VERSION}/auth`, auth);
+app.use(`/${BASE_URL}/${CURRENT_VERSION}/tools`, tool);
+app.use(`/${BASE_URL}/${CURRENT_VERSION}/papers`, papers);
+
 
 // Start the server on port 3000
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000.');
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 // Export the Express application. May be used by other modules. For example, API testing
