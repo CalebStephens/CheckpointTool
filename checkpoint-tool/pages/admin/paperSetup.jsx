ef3f185e5d8bb946601c1c5826cfe8176f393c64
CalebStephens <calebtaylorstephens@gmail.com>
2023-10-17 14:10:13 +1300

    add in new tabs along with ssr

--- checkpoint-tool/pages/admin/paperSetup.jsx
+++ checkpoint-tool/pages/admin/paperSetup.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 
 import Layout from "@/components/admin/layout";
 import StudentUpload from "@/components/admin/studentUpload";
-import AddLabs from "@/components/admin/addLabsTab";
+import LabAdmin from "@/components/admin/labAdmin";
+import ToolAdmin from "@/components/admin/toolAdmin";
 
 const PaperSetup = (props) => {
   const [currentTab, setCurrentTab] = useState("student");
@@ -11,7 +12,7 @@ const PaperSetup = (props) => {
   return (
     <Layout>
       <>
-        <div className="text-sm font-medium text-center text-white border-b border-gray-200 dark:text-white dark:border-gray-700">
+        <div className="text-sm font-medium text-center border-b border-gray-200 text-cyan-950 dark:border-gray-700">
           <ul className="flex flex-wrap -mb-px">
             <li className="mr-2" onClick={() => setCurrentTab("student")}>
               {currentTab == "student" ? (
@@ -39,10 +40,23 @@ const PaperSetup = (props) => {
                 </a>
               )}
             </li>
+            <li className="mr-2" onClick={() => setCurrentTab("tool")}>
+              {currentTab == "tool" ? (
+                <div
+                  className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                  aria-current="page">
+                  Tool
+                </div>
+              ) : (
+                <a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">
+                  Tool
+                </a>
+              )}
+            </li>
           </ul>
         </div>
 
-        {currentTab == "student" ? <StudentUpload students={props.students} /> : <> {currentTab == "labs" ? <AddLabs /> : <DeleteLab />} </>}
+        {currentTab == "student" ? <StudentUpload students={props.students} /> : <> {currentTab == "labs" ? <LabAdmin labs={props.labs}/> : <ToolAdmin/>} </>}
       </>
     </Layout>
   );
@@ -50,12 +64,16 @@ const PaperSetup = (props) => {
 
 export const getServerSideProps = async () => {
   const path = "http://localhost:3000/api/v1";
-  const resStudents = await axios.get(`${path}/students`);
+  const resStudents = await axios.get(`${path}/students?timestamp=${Date.now()}`);
   const students = resStudents.data.data;
-  console.log(students, "students");
+
+  const resLabs = await axios.get(`${path}/papers/labs/1?timestamp=${Date.now()}`);
+  const labs = resLabs.data.data;
+
   return {
     props: {
       students,
+      labs,
     },
   };
 };
