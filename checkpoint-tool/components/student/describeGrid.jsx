ff180692e2202e159837223ef21b341599c7296f
CalebStephens <calebtaylorstephens@gmail.com>
2023-11-02 21:22:36 +1300

    major refactor to include negative values alowing graphs to display correc data

--- checkpoint-tool/components/student/describeGrid.jsx
+++ checkpoint-tool/components/student/describeGrid.jsx
@@ -7,19 +7,29 @@ const DescribeGrid = (props) => {
 
   const rows = [];
 
-  const chooseCell = (cellKey) => 
-  {
+  const chooseCell = (cellKey) => {
     clearCell();
-    props.setCoords(cellKey.substr(2))
+    let coords = cellKey.substr(1).split("#");
+    coords = {
+      x: parseInt(coords[0]),
+      y: parseInt(coords[1]),
+    }
+    props.setCoords(coords);
     setSelectedCell(cellKey);
-    window.document.querySelector(`#${cellKey}`).style.backgroundColor = "#2463EB";
+    const cellElement = window.document.getElementById(cellKey); // Use getElementById
+    if (cellElement) {
+      cellElement.style.backgroundColor = "#2463EB";
+    }
 
-    console.log(cellKey.substr(2))
+    console.log(coords);
   };
 
   const clearCell = () => {
     if (selectedCell) {
-      window.document.querySelector(`#${selectedCell}`).style.backgroundColor = "inherit";
+      const selectedElement = window.document.getElementById(selectedCell); // Use getElementById
+      if (selectedElement) {
+        selectedElement.style.backgroundColor = "inherit";
+      }
       setSelectedCell(null);
     }
   };
@@ -32,34 +42,39 @@ const DescribeGrid = (props) => {
     resetCell();
   }, [props.reset]);
 
-  for (let row = 0; row < numRows; row++) {
+  for (let row = 10; row >= -10; row--) { // Reversed the row loop to start from 10 and go to -10
     const cells = [];
 
-    for (let col = 0; col < numCols; col++) {
-      const cellKey = `${row}-${col}`;
-      if (col === 10 || row === 10) {
-        
-        cells.push(<td key={cellKey}  className='cell w-1 h-1 bg-white border-2 border-white'></td>);
+    for (let col = -10; col <= 10; col++) {
+      const cellKey = `c${col}#${row}`; // Modify the format here
+      if (col === 0 || row === 0) {
+        cells.push(
+          <td key={cellKey} className="cell w-1 h-1 bg-white border-2 border-white"></td>
+        );
       } else {
         const cellStyles = {
-          width: "25px", 
-          height: "25px", 
+          width: "25px",
+          height: "25px",
         };
-        cells.push(<td key={cellKey} id={`c-${cellKey}`} style={cellStyles} className="border border-white" onClick={() => chooseCell(`c-${cellKey}`)}></td>);
+        cells.push(
+          <td
+            key={cellKey}
+            id={cellKey} // Modify the id attribute
+            style={cellStyles}
+            className="border border-white"
+            onClick={() => chooseCell(cellKey)} // Modify the parameter here
+          ></td>
+        );
       }
     }
     rows.push(
-      <tr className=" border-b bg-gray-800 border-gray-700" key={row}>
+      <tr className="border-b bg-gray-800 border-gray-700" key={row}>
         {cells}
       </tr>
     );
   }
 
-  return (
-
-      <tbody>{rows}</tbody>
-    
-  );
+  return <tbody>{rows}</tbody>;
 };
 
 export default DescribeGrid;
