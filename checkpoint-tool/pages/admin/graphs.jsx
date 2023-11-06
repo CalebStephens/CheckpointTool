dd3d1f5f610c80f2ac4d8180cce3a086db787c00
CalebStephens <calebtaylorstephens@gmail.com>
2023-11-06 21:34:31 +1300

    added tabs to page

--- checkpoint-tool/pages/admin/graphs.jsx
+++ checkpoint-tool/pages/admin/graphs.jsx
@@ -1,56 +1,91 @@
-import React, { useState } from "react";
-import ScatterGraph from "@/components/admin/ScatterGraph";
+import { useState } from "react";
+import Layout from "../../components/admin/layout";
+// import StudentScatterGraph from "../../components/admin/StudentScatterGraph";
+// import LabProgressionGraph from "../../components/admin/LabProgressionGraph";
 import { get } from "@/utils/api";
-import Layout from "@/components/admin/layout";
+import ScatterGraphParent from "@/components/admin/ScatterGraphParent";
 
-const Graphs = (props) => {
-  const [selectedLab, setSelectedLab] = useState(null);
-
-  const handlePaperSelect = (event) => {
-    const selectedValue = event.target.value;
-    setSelectedLab(selectedValue);
-  };
+const LabsCompletedPage = (props) => {
+  const [currentTab, setCurrentTab] = useState("labScatter");
 
   return (
     <Layout>
-      <div className="flex flex-col items-center p-4">
-        <h1 className="text-2xl font-semibold mb-4">Graphs</h1>
-
-        <div className="w-full max-w-md mb-4">
-          <select
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
-            onChange={handlePaperSelect}
-          >
-            <option value="">Select a Lab</option>
-            {props.paper.labs.map((lab, index) => (
-              <option key={index} value={lab.title}>
-                {lab.title}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {selectedLab && (
-          <ScatterGraph
-            students={props.paper.students}
-            tool={props.paper.tool}
-            labName={selectedLab}
-          />
-        )}
+      <h1 className="text-4xl m-4 font-extrabold text-cyan-950">Graphs</h1>
+      <div className="text-sm font-medium text-center border-b border-gray-200 text-cyan-950">
+        <ul className="flex flex-wrap -mb-px">
+          <li className="mr-2" onClick={() => setCurrentTab("labScatter")}>
+            {currentTab === "labScatter" ? (
+              <div
+                className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active hover:text-gray-600 hover:border-gray-300"
+                aria-current="page"
+              >
+                Lab Scatter
+              </div>
+            ) : (
+              <a
+                href="#"
+                className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+              >
+                Lab Scatter
+              </a>
+            )}
+          </li>
+          <li className="mr-2" onClick={() => setCurrentTab("studentScatter")}>
+            {currentTab === "studentScatter" ? (
+              <div
+                className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active hover:text-gray-600 hover:border-gray-300"
+                aria-current="page"
+              >
+                Student Scatter
+              </div>
+            ) : (
+              <a
+                href="#"
+                className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover-text-gray-600 hover:border-gray-300"
+              >
+                Student Scatter
+              </a>
+            )}
+          </li>
+          <li className="mr-2" onClick={() => setCurrentTab("labProgression")}>
+            {currentTab === "labProgression" ? (
+              <div
+                className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active hover-text-gray-600 hover:border-gray-300"
+                aria-current="page"
+              >
+                Lab Progression
+              </div>
+            ) : (
+              <a
+                href="#"
+                className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover-text-gray-600 hover:border-gray-300"
+              >
+                Lab Progression
+              </a>
+            )}
+          </li>
+        </ul>
+      </div>
+      <div>
+        {currentTab === "labScatter" && <ScatterGraphParent paper={props.paper} type={"Lab"} />}
+        {currentTab === "studentScatter" && <ScatterGraphParent paper={props.paper} type={"Student"} />}
+        {/* {currentTab === "studentScatter" && <StudentScatterGraph data={props.studentScatterData} />} */}
+        {/* {currentTab === "labProgression" && <LabProgressionGraph data={props.labProgressionData} />} */}
       </div>
     </Layout>
   );
 };
 
 export const getServerSideProps = async () => {
-  const res = await get(`papers/1?timestamp=${Date.now()}`);
-  const data = res.data.data;
 
-  return {
-    props: {
-      paper: data,
-    },
+    const res = await get(`papers/1?timestamp=${Date.now()}`);
+    const data = res.data.data;
+  
+    return {
+      props: {
+        paper: data,
+      },
+    };
   };
-};
 
-export default Graphs;
+export default LabsCompletedPage;
